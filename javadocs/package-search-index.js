0cd499f3a11d37383a9bd86353bef2cf71058384
Ribone <14286113+riboney@users.noreply.github.com>
2022-06-17 20:09:19 -0400

    Delete javadocs directory

--- javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.wgu.c482"},{"l":"org.wgu.c482.controllers"},{"l":"org.wgu.c482.models"},{"l":"org.wgu.c482.services"},{"l":"org.wgu.c482.utils"},{"l":"org.wgu.c482.views"},{"l":"org.wgu.c482.views.tables"},{"l":"org.wgu.c482.views.textfields"}]
